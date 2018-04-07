3402b802c11f900ebd2e9caa99d96a2e98863e75
muxe <mbarz@muxe.de>
2018-04-07 10:48:52 +0200

    restructured project for deployment with travis

---
+++ scripts/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# copy the public dir to the public dir on web
+echo 'copy public dir to server...'
+rsync -rav -e ssh --progress --delete --exclude multiLangPlan.json --exclude plan.json frontend/app/. muxe@schedar.uberspace.de:/home/muxe/html/futter/
+
+echo 'copy api logic'
+rsync -rav -e ssh --delete --exclude=node_modules backend/. muxe@schedar.uberspace.de:/home/muxe/canteenmenuparser
+ssh muxe@schedar.uberspace.de 'source .bash_profile && cd /home/muxe/canteenmenuparser && npm install --production'
+
+echo 'done. all files are copied to server. Nothing more to do.'
