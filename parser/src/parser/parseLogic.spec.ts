bb2e6afe28b2f9e9fa4a460b60b694d47a568103
mbarz <developer@muxe.de>
2019-04-17 16:53:53 +0200

    created test

--- parser/src/parser/parseLogic.spec.ts
+++ parser/src/parser/parseLogic.spec.ts
@@ -1,14 +1,17 @@
-import { writeFileSync } from "fs";
+import { writeFileSync, readFileSync } from "fs";
 import { PDFPlanParser } from "./pdf-plan-parser";
 
-const id = "bwg_a_de_001";
-const source = `${__dirname}/../../out/${id}.pdf`;
-const target = `${__dirname}/../../out/${id}.json`;
-
-async function test() {
-  const parser = new PDFPlanParser();
-  const result = await parser.parseFile(source);
-  writeFileSync(target, JSON.stringify(result, null, 2));
-}
-
-test();
+describe("parser", () => {
+  it("should parse", async () => {
+    const source = `res/example.pdf`;
+    const target = `out/test-result.json`;
+    const parser = new PDFPlanParser();
+    const result = await parser.parseFile(source);
+    writeFileSync(target, JSON.stringify(result, null, 2));
+    const actual = JSON.parse(readFileSync(target, "utf8"));
+    const expected = JSON.parse(
+      readFileSync("res/parsed-example.json", "utf8")
+    );
+    expect(actual).toEqual(expected);
+  });
+});
