5e73762d3cc1acc1a5028feaae82dcf953c9f57c
Max-Richard Barz <mbarz@muxe.de>
2019-07-01 15:11:32 +0200

    added one more fix

--- parser/src/parser/pdf-plan-reader.spec.ts
+++ parser/src/parser/pdf-plan-reader.spec.ts
@@ -27,14 +27,25 @@ describe("PDFPlanReader", () => {
     const reader = new PDFPlanReader();
     const days = reader.getTexts({
       texts: [
-        { R: [{ T: "2018-03-02" }], w: 0, x: 77, y: 6.8 },
+        {
+          x: 77.5792665627732,
+          y: 6.925,
+          w: 185.888,
+          R: [
+            {
+              T: "2019-07-08%20to%202019-07-12",
+              S: -1,
+              TS: [0, 19, 0, 0]
+            }
+          ]
+        },
         { R: [{ T: "GULASCH" }], w: 0, x: 1.95, y: 10.27 }
       ]
     });
     expect(days.length).toEqual(1);
     expect(days[0]).toEqual({
       name: "Montag",
-      date: "2018-03-02",
+      date: "2019-07-08",
       meals: [{ lines: ["GULASCH"] }]
     });
   });
